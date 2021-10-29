64e1b114975c81290a93a5d7f317ef36353b3dfa
Ying Qi Tay <yingqi99@hotmail.com>
2021-10-29 21:07:17 +0800

    update to touchgui

--- fish_init/sketch.js
+++ fish_init/sketch.js
@@ -3,34 +3,88 @@ colorarray = ['#b04119','yellow','blue'];
 let fish;
 let v;  //translation vector for fish to move wrt joystick
 
-var buttonList = [];
-let button_enclosure;
-let button_joystick;
-let button_feed
+let gui;
+let joystick;
+let feedButton;
+let enclosureButton;
+let col = 255;  //base col button white
 
 var foodArr = [];
 
-function touchMove(event){
-  event.preventDefault();
-}
-
-function gestureChange(event){
-  event.preventDefault();
-}
 
 function setup() {
   // setInterval(spawn,1000)
-  createCanvas(1024, 768);
+  // createCanvas(1024, 6/8*768);
+  createCanvas(1536,864);//acer aspire14 aspect ratio
   // frameRate(10);
   
   fish = new Fish(width/2-20, height/2, colorarray[1], 0,90);
   fisharray.push(fish);
   
-  button_enclosure = new Button("enclosure", "press", width/2-20, height/2, 200);
-  button_joystick = new Button("joystick", "joystick", width-180, height-160, 100);
-  button_feed = new Button("feed", "press", 120, 120, 100);
+  gui = createGui();
   
-  buttonList.push(button_enclosure, button_joystick, button_feed);
+  // Create Joystick.
+  // The last four optional arguments define minimum and maximum values 
+  // for the x and y axes; minX, maxX, minY, maxY
+  // The default min and max values for all four are -1 and 1.
+  joystick = createJoystick("Joystick", width*6.5/8, height*2.8/4, 175, 175, -1, 1, 1, -1);
+  joystick.setStyle({
+    fillBg: color(col,30),
+    fillBgHover: color(col,30),
+    fillBgActive: color(col,60),
+    strokeBg: color(col,50),  //nostroke
+    strokeBgHover: color(col,50),  //nostroke
+    // strokeWeight: 10,
+    rounding: 87.5,  //change for circle
+    fillHandle: color(col,50)
+  });
+
+  
+  //create feed button
+  feedButton = createButton("", 220, 130, 100, 100);
+  feedButton.setStyle({
+    fillBg: color(col,30),
+    fillBgHover: color(col,30),
+    fillBgActive: color(col,80),
+    strokeBg: color(col,50),  //nostroke
+    strokeBgHover: color(col,50),  //nostroke
+    strokeBgActive: color(col,80),  //nostroke
+    // strokeWeight: 10,
+    rounding: 50,  //change for circle
+  });
+  feedButton.onPress = function(){
+     for(let i=0; i<random(20,40); i++){
+          let food = new Food(random(10,20), colorarray[2]);
+          foodArr.push(food);
+        }
+        print("feed");
+  }
+  
+    //create enclosure button
+  enclosureButton = createButton("", width/2-100, height/2-100, 200, 200);
+  enclosureButton.setStyle({
+    fillBg: color(col,30),
+    fillBgHover: color(col,30),
+    fillBgActive: color(col,80),
+    strokeBg: color(col,50),  //nostroke
+    strokeBgHover: color(col,50),  //nostroke
+    strokeBgActive: color(col,80),  //nostroke
+    // strokeWeight: 10,
+    rounding: 100,  //change for circle
+  });
+  enclosureButton.onPress = function(){
+    if(fish.escape){
+      fish.x = fish.x_init;
+      fish.y = fish.y_init;
+      fish.escape = false;
+    }  else{
+      fish.peek = true;
+    }
+  }
+
+  enclosureButton.onRelease = function(){
+    fish.peek = false;
+  }
 }
 
 class Fish {
@@ -43,7 +97,7 @@ class Fish {
     this.speedX = speed;
     this.speedY = speed;
     this.size = size;
-    // this.vTranslate = createVector(0,0);
+    this.drxn = "left";  //direction faced
     
     this.peek = false;
     this.escape = false;
@@ -51,29 +105,23 @@ class Fish {
   
 
   //movement
-  swim(){    
+  swim(){
     //set x bound
     if(this.x-this.size/2<0){
       this.x = this.size/2;
-    }else if(this.x+80 > width){
+    }else if(this.x+80>width){
       this.x = width-80;
     }
     //set y bound
     if(this.y-this.size/4<0) {
        this.y = this.size/4;
      }else if(this.y + this.size/4 > height){
-       this.y = height - this.size/4
+       this.y = height - this.size/4;
      }
     
     if(this.escape){  //escape enclosure
-      if(v){
-        let vector = v.normalize();
-        this.speedX = 5*vector.x;
-        this.speedY = 5*vector.y;
-      }else{
-        this.speedX = 0;
-        this.speedY = 0;
-      }
+      this.speedX = 5*joystick.valX;
+      this.speedY = 5*joystick.valY;
       
     }else{  //within enclosure
       if(this.peek){
@@ -82,146 +130,54 @@ class Fish {
         if(this.x == this.x_init-50 && this.y == this.y_init-50){
           this.speedX = 0;
           this.speedY = 0;
+          // this.speedX = 5*joystick.valX;
+          // this.speedY = 5*joystick.valY;
+          if(joystick.isPressed){
+            this.escape = true;
+          }
         }
       }else{
         this.speedX = 2;
         this.speedY = 2;
-        if(this.x == this.x_init && this.y == this.y_init){
+        if(this.x == this.x_init || this.y == this.y_init){
+          this.x = this.x_init;
+          this.y = this.y_init;
           this.speedX = 0;
           this.speedY = 0;
-          if(v != null && v.x<0 ){
-            this.escape = true;
-            print("escape")
-          }
         }
       }
     }
-    
+
     this.x += this.speedX;
     this.y += this.speedY;
   }
   
   drawFish(){
+    // push();
   fill(this.filler);
+    //body
     ellipse(this.x,this.y,this.size,this.size-((this.size)/2));
-    triangle(this.x+30,this.y,this.x+80,this.y-30,this.x+80,this.y+30);
-  fill(20);
-    ellipse(this.x-20,this.y,10,10);
+    //tail
+    if(this.drxn == "left"){
+      triangle(this.x+30,this.y,this.x+80,this.y-30,this.x+80,this.y+30);
+    fill(20);
+      //eyes
+      ellipse(this.x-20,this.y,10,10);
+    }else{
+      //tail
+      triangle(this.x-30,this.y,this.x-80,this.y-30,this.x-80,this.y+30);
+    fill(20);
+      //eyes
+      ellipse(this.x+20,this.y,10,10);
+    }
+    
+    
     // pop();
     // fill(100, 80)
     // rect(this.x-this.size/2, this.y-this.size/4, this.size, this.size/2);// body region
   }
   
 }
-
-// function spawn(){
-//   var h = new Fish(random(500,700),random(1,400),random(colorarray),random(1,4),random(90,130))
-//   fisharray.push(h)
-// }
-
-class Button {
-  constructor(name, type, x, y, size){
-    this.name = name;
-    this.type = type;
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    
-    this.pressed = false;
-  }
-  
-  display(){
-    let baseColor = color(250,80);
-    if(this.type == "press"){
-      stroke(baseColor);
-      strokeWeight(5);
-      noFill();
-      ellipse(this.x, this.y , this.size, this.size);
-    } else if(this.type == "joystick"){
-      stroke(baseColor);
-      strokeWeight(5);
-      noFill();
-      let outerDiameter = this.size *1.5;
-      //outer rim
-      ellipse(this.x, this.y , outerDiameter, outerDiameter);
-      
-      noStroke();
-      if(this.pressed){
-        fill(250);
-      }else{
-        fill(baseColor);
-      }
-      //joystick moving part
-      v = null;
-      push();
-      if(this.pressed){
-        let d;
-        d = dist(this.x, this.y, mouseX, mouseY);
-        v = createVector(mouseX-this.x, mouseY-this.y);
-        if(d>this.size){
-          v = v.normalize() * this.size;
-          
-        // print(v);
-        }    
-        translate(v);
-      }
-      ellipse(this.x, this.y , this.size, this.size);
-      pop();
-    }
-  }
-  
-  checkPressed(touch){
-    if(touch.x>this.x-this.size/2 && touch.x<this.x+this.size/2 && touch.y>this.y-this.size/2 && touch.y<this.y+this.size/2){
-      this.pressed = true;
-      
-      if(this.name == "enclosure"){
-        fish.peek = true;
-        print("peek");
-      } else if(this.name == "feed"){
-        for(let i=0; i<random(20,40); i++){
-          let food = new Food(random(10,20), colorarray[2]);
-          foodArr.push(food);
-        }
-        print("feed");
-      }
-
-    }
-    
-    if(this.type == "joystick"){
-      if(touch.x>this.x-this.size && touch.x<this.x+this.size && touch.y>this.y-this.size && touch.y<this.y+this.size){
-        this.pressed = true;
-
-      }
-    }
-  }
-  
-  checkReleased(){
-    if(this.pressed){
-    this.pressed = false;
-      if(this.name == "enclosure"){
-        fish.peek = false;
-        print("return");
-      }
-    }
-    // if(mouseX>=this.x-this.size/2 && mouseX<=this.x+this.size/2 && mouseY>=this.y-this.size/2 && mouseY<=this.y+this.size/2){
-    //   this.pressed = false;
-    //   if(this.name == "enclosure"){
-    //     fish.peek = false;
-    //     print("return");
-    //   }
-    // }
-    
-//     if(this.type == "joystick"){
-//       if(mouseX>this.x-this.size && mouseX<this.x+this.size && mouseY>this.y-this.size && mouseY<this.y+this.size){
-//         this.pressed = false;
-
-//       }
-//     }
-  }
-  
-}
-
-
 
 class Food{
   constructor(size, color){
@@ -245,66 +201,27 @@ class Food{
   }
 }
 
-// function touchStarted(){
-//   for(var button of buttonList){
-//       button.checkPressed();
-//     }
-//   // for(var i=0; i < touches.length; i++){
-//   //   print("checking")
-//   //   let touch = touches[i];
-//   //     for(var button of buttonList){
-//   //     button.checkPressed();
-//   //       print("checking")
-//   //   }
-//   // }
-  
-  
-//   return false;
-// }
+//fullscreen - doesnt work on ios...
+function touchStarted () {
+  var fs = fullscreen();
+  if (!fs) {
+    fullscreen(true);
+  }
+}
 
-
-
-// //fullscreen - doesnt work on ios...
-// function touchStarted () {
-//   var fs = fullscreen();
-//   if (!fs) {
-//     fullscreen(true);
-//   }
-// }
-
-// //fullscreen not working on ios
 // /* full screening will change the size of the canvas */
 // function windowResized() {
 //   resizeCanvas(windowWidth, windowHeight);
+//   // print(windowWidth, windowHeight)
 // }
 
-function touchEnded(){
-  for(var button of buttonList){
-      button.checkReleased();
-    }
-  for(var i=0; i < touches.length; i++){
-    let touch = touches[i];
-    
-  }
-  
-  return false;
-}
-
-
-
 function draw() {
-  background(0);
+  background(0,0,100);
   
-  for(var i=0; i < touches.length; i++){
-    let touch = touches[i];
-      for(var button of buttonList){
-      button.checkPressed(touch);
-        print("checking")
-    }
-  }
-  
-  for(var button of buttonList){
-    button.display();
+  if(joystick.valX<0){
+    fish.drxn = "left";
+  } else if(joystick.valX>0) {
+    fish.drxn = "right";
   }
   
   for(var i=0; i<foodArr.length; i++){
@@ -312,9 +229,9 @@ function draw() {
     food.spawn();
     
     //remove food when overlap with fish body
-    //remove food when out of bounds
+    //remove food when oob
     if(food.x > fish.x-fish.size/2 && food.x < fish.x+fish.size/2 && food.y > fish.y-fish.size/4 && food.y < fish.y+fish.size/4 ||
-      food.x>width || food.y > height){
+      food.x > width || food.y > height){
       foodArr.splice(i,1);
     }
 
@@ -327,5 +244,6 @@ function draw() {
   //   h.drawFish()
   //   h.swim()
   // }
+  drawGui();
 
 }
